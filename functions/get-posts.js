a4e65f072e20fa450e24d84ed9ee3493b9ebab8e
chienlove <mobile@ip6-dynamic-adsl.viettel.vn>
2024-07-05 20:18:22 +0700

    Changed 5 files Modified: js/all.js Deleted: - functions/create-post.js ,- functions/get-posts.js ,- netlify.toml ,- netlify/functions/createPost.js

--- functions/get-posts.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async (event, context) => {
-    try {
-        // Ở đây, bạn sẽ lấy danh sách bài viết từ cơ sở dữ liệu hoặc CMS của bạn
-        // Đây chỉ là một ví dụ đơn giản
-        const posts = [
-            { id: 1, title: 'Bài viết 1' },
-            { id: 2, title: 'Bài viết 2' },
-            { id: 3, title: 'Bài viết 3' },
-        ];
