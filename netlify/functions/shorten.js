3651f344c54b555510052df8ddaf307e08e192f6
chienlove <mobile@ip6-dynamic-adsl.viettel.vn>
2024-07-29 00:17:17 +0700

    Changed 3 files Modified: layouts/shortener/shortener.html,netlify.toml,netlify/functions/shorten.js

--- netlify/functions/shorten.js
+++ netlify/functions/shorten.js
@@ -1,42 +1,42 @@
-const faunadb = require('faunadb')
+const faunadb = require('faunadb');
 
-const q = faunadb.query
+const q = faunadb.query;
 const client = new faunadb.Client({
   secret: process.env.FAUNA_SECRET_KEY
-})
+});
 
 exports.handler = async (event) => {
   try {
-    const { url } = JSON.parse(event.body)
+    const { url } = JSON.parse(event.body);
     if (!url) {
       return {
         statusCode: 400,
         body: JSON.stringify({ error: 'URL is required' })
-      }
+      };
     }
 
-    const shortId = generateShortId()
-    const response = await client.query(
+    const shortId = generateShortId();
+    await client.query(
       q.Create(q.Collection('links'), {
         data: { url, shortId }
       })
-    )
+    );
 
-    const siteUrl = process.env.SITE_URL || 'http://localhost:8888' // Sử dụng URL cục bộ nếu biến môi trường không có
+    const siteUrl = process.env.SITE_URL || 'http://localhost:8888'; // Sử dụng URL cục bộ nếu biến môi trường không có
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ shortUrl: `${siteUrl}/.netlify/functions/redirect/${shortId}` })
-    }
+      body: JSON.stringify({ shortUrl: `${siteUrl}/${shortId}` })
+    };
   } catch (error) {
-    console.error('Error:', error)
+    console.error('Error:', error);
     return {
       statusCode: 500,
       body: JSON.stringify({ error: 'Internal Server Error' })
-    }
+    };
   }
-}
+};
 
 function generateShortId() {
-  return Math.random().toString(36).substr(2, 8)
+  return Math.random().toString(36).substr(2, 8);
 }
