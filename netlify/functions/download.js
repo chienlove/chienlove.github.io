8f023ef58b149ec1e23fca4730173842096dc344
chienlove <mobile@iPhone>
2024-08-05 05:36:23 +0700

    Changed 9 files Added: layouts/intermediate.html,layouts/jailbreak-tools/single.html,netlify/functions/download.js,static/_redirects,static/css/intermediate.css,static/css/jailbreak-tools.css Modified: layouts/upfile/uploads.html,static/admin/config.yml Deleted: - netlify/functions/test-fauna.js

---
+++ netlify/functions/download.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const path = require('path');
+const matter = require('gray-matter');
+
+exports.handler = async function(event, context) {
+  const appId = event.queryStringParameters.appId;
+  const contentDir = path.join(__dirname, '../../content/jailbreak-tools');
+
+  try {
+    const files = fs.readdirSync(contentDir);
+    for (const file of files) {
+      const filePath = path.join(contentDir, file);
+      const fileContents = fs.readFileSync(filePath, 'utf8');
+      const { data } = matter(fileContents);
+
+      for (const version of data.versions) {
+        if (version.appId === appId) {
+          return {
+            statusCode: 302,
+            headers: {
+              Location: version.intermediate_page_url.replace(':appId', appId)
+            }
+          };
+        }
+      }
+    }
+
+    return {
+      statusCode: 404,
+      body: 'App not found'
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: 'Internal Server Error'
+    };
+  }
+};
