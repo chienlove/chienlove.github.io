22102ce04e7944a62eb195472fbc349c8bbfdc08
chienlove <mobile@ip6-dynamic-adsl.viettel.vn>
2024-08-08 12:10:11 +0700

    Modified: netlify-build.sh

--- netlify-build.sh
+++ netlify-build.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+# Cập nhật và cài đặt libimobiledevice-utils
+apt-get update
+apt-get install -y libimobiledevice-utils
+
+# Cài đặt Homebrew và các công cụ cần thiết
+brew install atool
+
+# Tải xuống và cài đặt ipatool
+git clone https://github.com/majd/ipatool.git
+cd ipatool
+make install
+cd ..
+
 # Chạy Hugo build
 hugo --gc --minify
-
-# Cài đặt ipatool qua Homebrew
-brew tap majd/repo
-brew install ipatool
