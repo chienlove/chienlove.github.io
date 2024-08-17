7bfef8fa4e63991040e1823fccb6723b70c54a4a
chienlove <mobile@ip6-dynamic-adsl.viettel.vn>
2024-08-18 00:49:16 +0700

    Modified: netlify/functions/check-status.js

--- netlify/functions/check-status.js
+++ netlify/functions/check-status.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 
 exports.handler = async function(event, context) {
-  const url = 'https://claude.ai'; 
+  const url = 'https://ipa-apps.me'; 
   
   try {
     const response = await fetch(url);
