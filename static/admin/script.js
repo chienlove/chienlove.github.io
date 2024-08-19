71bd87ea93cbbbda3b4ec9c0fc4a065ebb914ccd
chienlove <mobile@ip6-dynamic-adsl.viettel.vn>
2024-08-19 22:24:58 +0700

    Changed 3 files Added: static/admin/dashboard.html,static/admin/script.js Modified: static/admin/index.html

---
+++ static/admin/script.js
@@ -0,0 +1,8 @@
+// admin-custom.js
+document.addEventListener('DOMContentLoaded', function() {
+  // Thay đổi giao diện hoặc thêm các tính năng
+  console.log('Custom admin script loaded');
+  
+  // Ví dụ: Thay đổi màu nền của trang admin
+  document.querySelector('body').style.backgroundColor = '#f0f0f0';
+});
