bd1533488acd2cf73970dc55a093985ae65072d4
chienlove <mobile@ip6-dynamic-adsl.viettel.vn>
2024-08-23 21:17:41 +0700

    Changed 3 files Modified: layouts/_default/baseof.html,static/css/style.css,static/js/main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,16 +1,9 @@
-document.querySelector('.hamburger-menu').addEventListener('click', function() {
-    this.classList.toggle('active');
-    document.querySelector('.mobile-nav').classList.toggle('show');
+document.querySelector('.hamburger-menu').addEventListener('click', function () {
+    document.querySelector('.mobile-nav').classList.toggle('active');
+    document.querySelector('.sidebar-overlay').classList.toggle('active');
 });
 
-function loginAsAdmin() {
-    // Logic to handle admin login
-}
-
-function logout() {
-    // Logic to handle logout
-}
-
-function loginWithGoogle() {
-    // Logic to handle Google login
-}
+document.querySelector('.sidebar-overlay').addEventListener('click', function () {
+    document.querySelector('.mobile-nav').classList.remove('active');
+    document.querySelector('.sidebar-overlay').classList.remove('active');
+});
