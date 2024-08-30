6a632f4ce9ece6a83e5082b98bd5bcc46b10b30f
chienlove <mobile@ip6-dynamic-adsl.viettel.vn>
2024-08-30 11:13:43 +0700

    Modified: netlify/functions/check-status.js

--- netlify/functions/check-status.js
+++ netlify/functions/check-status.js
@@ -1,14 +1,14 @@
 const fetch = require('node-fetch');
 
 exports.handler = async function(event, context) {
-  const url = 'https://ipa-apps.me'; 
+  const url = 'https://ipa-apps.me';
   
   try {
-    const response = await fetch(url);
+    const response = await fetch(url, { headers: { 'Cache-Control': 'no-cache' } });
     const text = await response.text();
 
     let status = 'revoked';
-    if (text.includes('SIGNED')) {
+    if (text.toLowerCase().includes('signed')) {
       status = 'signed';
     }
 
