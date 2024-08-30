5c80019bc6c220fe2af018caf47a867ce4c93a88
chienlove <mobile@ip6-dynamic-adsl.viettel.vn>
2024-08-30 11:26:48 +0700

    Changed 2 files Modified: netlify/functions/check-status.js,package.json

--- netlify/functions/check-status.js
+++ netlify/functions/check-status.js
@@ -1,10 +1,20 @@
-const puppeteer = require('puppeteer');
+const chromium = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer-core');
 
 exports.handler = async function(event, context) {
   const url = 'https://ipa-apps.me';
-  
+
+  let browser = null;
+
   try {
-    const browser = await puppeteer.launch();
+    // Khởi chạy trình duyệt headless với chrome-aws-lambda
+    browser = await puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+    });
+
     const page = await browser.newPage();
     await page.goto(url, { waitUntil: 'networkidle2' });
 
@@ -24,7 +34,11 @@ exports.handler = async function(event, context) {
   } catch (error) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: 'Error fetching data' }),
+      body: JSON.stringify({ error: 'Error fetching data', details: error.message }),
     };
+  } finally {
+    if (browser) {
+      await browser.close();
+    }
   }
 };
