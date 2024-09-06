05868f8fcee1eb78b8d229b16c458cb8ade3b6cf
chienlove <mobile@iPhone>
2024-09-06 09:51:14 +0700

    Modified: netlify/functions/update-plists.js

--- netlify/functions/update-plists.js
+++ netlify/functions/update-plists.js
@@ -17,7 +17,8 @@ exports.handler = async function(event, context) {
     'https://file.jb-apps.me/plist/FilzaEscaped15.plist': 'static/plist/filzaescaped15.plist',
     'https://file.jb-apps.me/plist/Taurine.plist': 'static/plist/taurine.plist',
     'https://file.jb-apps.me/plist/NekoJB.plist': 'static/plist/neko.plist',
-    'https://file.jb-apps.me/plist/ChimeraJB.plist': 'static/plist/chimera.plist'
+    'https://file.jb-apps.me/plist/ChimeraJB.plist': 'static/plist/chimera.plist',
+    'https://file.jb-apps.me/plist/Odyssey.plist': 'static/plist/odyssey.plist'
   };
 
   let updatesPerformed = false;
