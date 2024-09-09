c37cd6b1f9db806779430319e77f8742a7cacaf9
chienlove <mobile@iPhone>
2024-09-09 21:12:00 +0700

    Modified: static/admin/widgets/updateSize.js

--- static/admin/widgets/updateSize.js
+++ static/admin/widgets/updateSize.js
@@ -29,10 +29,11 @@ CMS.registerWidget('update-size', createClass({
               // Cập nhật giá trị vào trường kích thước file (size)
               console.log('Updating size:', data.size);
 
-              // Sử dụng API của CMS để cập nhật trường trong form
-              this.props.onChange(
-                this.props.entry.get('data').set('size', data.size)
-              );
+              // Cập nhật trường widget "size" bằng cách sử dụng API của CMS
+              this.props.entry.get('data').set('size', data.size);
+              
+              // Gọi lại hàm onChange để cập nhật giá trị trong CMS
+              this.props.onChange(this.props.entry.get('data').set('size', data.size));
               
               alert('Kích thước đã được cập nhật: ' + data.size);
             } else {
