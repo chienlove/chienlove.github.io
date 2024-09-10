6288da0c1e48566d581d0412021a7e512e05fb0e
chienlove <mobile@ip6-dynamic-adsl.viettel.vn>
2024-09-10 22:18:51 +0700

    Modified: static/admin/widgets/updateSize.js

--- static/admin/widgets/updateSize.js
+++ static/admin/widgets/updateSize.js
@@ -1,6 +1,8 @@
 CMS.registerWidget('update-size', createClass({
   handleClick() {
-    const mainDownload = this.props.entry.getIn(['data', 'main_download']);
+    const entry = this.props.entry;
+    const collection = this.props.collection;
+    const mainDownload = entry.getIn(['data', 'main_download']);
     const plistUrl = mainDownload ? mainDownload.get('plistUrl') : null;
 
     if (!plistUrl) {
@@ -19,8 +21,9 @@ CMS.registerWidget('update-size', createClass({
       })
       .then(data => {
         if (data.size) {
-          // Cập nhật giá trị của trường
-          this.props.onChange(data.size);
+          // Cập nhật giá trị của trường size trong entry
+          const newEntry = entry.setIn(['data', 'size'], data.size);
+          this.props.onChange(newEntry);
           alert('Kích thước đã được cập nhật: ' + data.size);
         } else {
           throw new Error('Không nhận được dữ liệu kích thước từ API.');
@@ -36,13 +39,18 @@ CMS.registerWidget('update-size', createClass({
   },
 
   render() {
+    const entry = this.props.entry;
+    const size = entry.getIn(['data', 'size']);
     const loading = this.state?.loading;
 
     return h('div', {},
       h('input', {
         type: 'text',
-        value: this.props.value || '',
-        onChange: e => this.props.onChange(e.target.value),
+        value: size || '',
+        onChange: e => {
+          const newEntry = entry.setIn(['data', 'size'], e.target.value);
+          this.props.onChange(newEntry);
+        },
         disabled: loading,
         style: { marginRight: '10px' }
       }),
