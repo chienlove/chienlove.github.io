541d9caa259ec94de69df5e05b75fbd1325aa1d4
chienlove <mobile@ip6-dynamic-adsl.viettel.vn>
2024-09-17 21:24:47 +0700

    Modified: netlify/functions/zalovideo.js

--- netlify/functions/zalovideo.js
+++ netlify/functions/zalovideo.js
@@ -1,7 +1,8 @@
 const axios = require('axios');
 
 async function fetchVideoSuggestions() {
-  const url = 'https://api.zalo.video/v1/home/trending/get/list_video_suggestion?cTime=1726582327357&sig=b23419569c62b294f85b215c4ff606ca';
+  const cTime = Date.now(); // Lấy thời gian hiện tại
+  const url = `https://api.zalo.video/v1/home/trending/get/list_video_suggestion?cTime=${cTime}&sig=b23419569c62b294f85b215c4ff606ca`;
   
   try {
     const response = await axios.get(url);
