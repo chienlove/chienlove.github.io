a8798e9a8e7e8ae35c141dfdda59fa999f28d453
chienlove <37842794+chienlove@users.noreply.github.com>
2025-07-02 19:27:45 +0700

    .

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { supabase } from '../lib/supabase';
 import Layout from '../components/Layout';
