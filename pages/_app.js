0501549707c4e0ea3e67292ccac2ad4d2fc9035a
chienlove <37842794+chienlove@users.noreply.github.com>
2025-07-02 13:55:35 +0700

    .

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
+// pages/_app.js
 import '../styles/globals.css';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
+
+export default MyApp;
