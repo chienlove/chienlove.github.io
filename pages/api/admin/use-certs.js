3210b6ed44fa52af54b5fe6570de940020ffaa14
chienlove <37842794+chienlove@users.noreply.github.com>
2025-07-05 07:10:30 +0700

    .

--- pages/api/admin/use-certs.js
+++ pages/api/admin/use-certs.js
@@ -131,7 +131,7 @@ export default async function handler(req, res) {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        ref: 'main',
+        ref: 'master',
         inputs: { tag, identifier },
       }),
     });
