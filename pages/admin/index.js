e09b88228baddbac4ee854f197f4bcd4ea310a36
chienlove <37842794+chienlove@users.noreply.github.com>
2025-07-07 08:09:19 +0700

    .

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 import { supabase } from "../../lib/supabase";
 import { v4 as uuidv4 } from 'uuid';
 import CertUploader from '../../components/admin/CertUploader';
+import CertManagerAndSigner from "../../components/admin/CertManagerAndSigner";
 
 export default function Admin() {
   const router = useRouter();
