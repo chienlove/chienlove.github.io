b521d3a998630521d8c40cff9053ee92c021c9d6
chienlove <37842794+chienlove@users.noreply.github.com>
2025-07-11 09:05:08 +0700

    .

--- pages/[slug].js
+++ pages/[slug].js
@@ -175,7 +175,7 @@ export default function Detail() {
 
     {statusLoading || status === null ? (
       <span className="inline-block border border-gray-300 text-gray-500 bg-gray-50 px-4 py-2 rounded-full text-sm font-semibold">
-        Đang kiểm tra trạng thái...
+        Loading...
       </span>
     ) : (
       <>
