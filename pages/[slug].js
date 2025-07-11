f3bc1dd013f82be65f9333c2162e3b2d8e5359d1
chienlove <37842794+chienlove@users.noreply.github.com>
2025-07-11 08:21:23 +0700

    .

--- pages/[slug].js
+++ pages/[slug].js
@@ -179,13 +179,13 @@ export default function Detail() {
       </span>
     )}
     {status === 'F' && (
-      <span className="inline-block border border-yellow-500 text-yellow-700 bg-yellow-50 px-4 py-2 rounded-full text-sm font-semibold">
+      <span className="inline-block border border-red-500 text-red-700 bg-red-50 px-4 py-2 rounded-full text-sm font-semibold">
         <FontAwesomeIcon icon={faExclamationTriangle} className="mr-1" />
         Đã đầy
       </span>
     )}
     {status === 'N' && (
-      <span className="inline-block border border-red-500 text-red-700 bg-red-50 px-4 py-2 rounded-full text-sm font-semibold">
+      <span className="inline-block border border-yellow-500 text-yellow-700 bg-yellow-50 px-4 py-2 rounded-full text-sm font-semibold">
         <FontAwesomeIcon icon={faTimesCircle} className="mr-1" />
         Ngừng nhận
       </span>
