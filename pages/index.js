b87f2660f65312a3384482d6a05c99d6dd2d4855
chienlove <37842794+chienlove@users.noreply.github.com>
2025-08-18 22:04:29 +0700

    .

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home({ categoriesWithApps, certStatus }) {
     <Layout>
       <div className="container mx-auto px-1 md:px-2 py-6 space-y-10">
         {/* Banner đầu trang */}
-        <AdUnit mobileSlot="5160182988" desktopSlot="4575220124" />
+        <AdUnit mobileSlot1="5160182988" mobileSlot2="7109430646" />
 
         {categoriesWithApps.map((category, index) => (
           <div
@@ -72,13 +72,13 @@ export default function Home({ categoriesWithApps, certStatus }) {
 
             {/* Banner giữa trang: sau mỗi 2 danh mục và giới hạn để tránh dày đặc */}
             {((index + 1) % 2 === 0) && (index < 4) && (
-              <AdUnit mobileSlot="5160182988" desktopSlot="4575220124" />
+              <AdUnit mobileSlot1="5160182988" mobileSlot2="7109430646" />
             )}
           </div>
         ))}
 
         {/* Banner cuối trang */}
-        <AdUnit mobileSlot="5160182988" desktopSlot="4575220124" />
+        <AdUnit mobileSlot1="5160182988" mobileSlot2="7109430646" />
       </div>
     </Layout>
   );
