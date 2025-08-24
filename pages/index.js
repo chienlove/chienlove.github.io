8ceb08c8ca084cb59522ed4bb106ba1fd42100c0
chienlove <37842794+chienlove@users.noreply.github.com>
2025-08-25 04:20:21 +0700

    .

--- pages/index.js
+++ pages/index.js
@@ -145,8 +145,9 @@ export default function Home({ categoriesWithApps, hotApps, paginationData }) {
           <div className={contentCard}>
             <div className="flex items-center gap-3 mb-4">
               <FontAwesomeIcon icon={faFire} className="text-2xl text-red-500" />
-              <h2 class="text-2xl font-bold text-emerald-600 dark:text-emerald-400">Ứng dụng tải/xem nhiều
+              <h2 class="text-2xl font-bold text-emerald-600 dark:text-emerald-400">Top download
               </h2>
+              <FontAwesomeIcon icon={faFire} className="text-2xl text-red-500" />
             </div>
             <div className="space-y-1">
               {hotApps.map((app, index) => (
