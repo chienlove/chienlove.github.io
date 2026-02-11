69bb2eaf6b66a520e077ee4931807512c846f414
chienlove <37842794+chienlove@users.noreply.github.com>
2026-02-11 18:50:10 +0700

    .

--- pages/api/admin/list-certs.js
+++ pages/api/admin/list-certs.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
 export default async function handler(req, res) {
